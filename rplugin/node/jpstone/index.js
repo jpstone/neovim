1775b2c2fabee45247d362c8a640665af7c33cc9
Justin Stone <jstone@hanoverresearch.com>
2017-12-27 10:29:36 -0500

    update

--- rplugin/node/jpstone/index.js
+++ rplugin/node/jpstone/index.js
@@ -1,15 +1,40 @@
+const fs = require('fs');
+const path = require('path');
 const { Plugin, Command, Autocmd } = require('neovim');
 
+const a = __dirname;
+
+const nvimDir = path.resolve('~/.local/share/nvim');
+const pluginPath = path.join(nvimDir, 'plugged');
+const plugins = [
+	'scrooloose/nerdtree',
+];
+
+function getPluginDir(p) {
+	return path.join(pluginPath, p.split('/')[1]);
+}
+
+function doesExist({ isDirectory }) {
+	return isDirectory();
+}
+
 class ConfigPlugin {
-  init() {
+  loadPlugins() {
+		let _isInstalled = true;
+    this.nvim.command(`call plug#begin(${pluginPath})`);
+		plugins.forEach(p => this.nvim.command(`Plug ${p}`));
+		plugins.forEach((p, i) => fs.stat(getPluginDir(p), (err, stat) => {
+			if (!doesExist(stat)) _isInstalled = false;
+			if (i === plugins.length - 1 && !_isInstalled) return this.nvim.command('PluginInstall');
+			console.log('All plugins loaded');
+		}));
   }
 
   autorun() {
-    this.nvim.command('JPStoneInit');
+		this.loadPlugins();
   }
 }
 
-Command('JPStoneInit')(ConfigPlugin.prototype.init);
 Autocmd('VimEnter', { pattern: '*', sync: true })(ConfigPlugin.prototype.autorun);
 
 module.exports = Plugin({ dev: true })(ConfigPlugin);
